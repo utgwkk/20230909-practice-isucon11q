864e8fe9aa3e5c3273a17adaa345c540c56049c5
utgwkk <utagawakiki@gmail.com>
2023-09-09 17:44:40 +0900

    300

--- go/main.go
+++ go/main.go
@@ -291,8 +291,8 @@ func main() {
 		e.Logger.Fatalf("failed to connect db: %v", err)
 		return
 	}
-	db.SetMaxOpenConns(200)
-	db.SetMaxIdleConns(200)
+	db.SetMaxOpenConns(300)
+	db.SetMaxIdleConns(300)
 	defer db.Close()
 
 	postIsuConditionTargetBaseURL = os.Getenv("POST_ISUCONDITION_TARGET_BASE_URL")
