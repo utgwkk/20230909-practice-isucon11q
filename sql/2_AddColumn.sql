c313509a3585567feab9fd84260d8efea8af7000
utgwkk <utagawakiki@gmail.com>
2023-09-09 13:49:04 +0900

    update initial data condition_level

--- sql/2_AddColumn.sql
+++ sql/2_AddColumn.sql
@@ -1,1 +1,9 @@
 ALTER TABLE `isu_condition` ADD COLUMN `condition_level` ENUM('info', 'warning', 'critical') NOT NULL AFTER `condition`;
+UPDATE `isu_condition` SET `condition_level` = (
+    CASE (LENGTH(`condition`) - LENGTH(REPLACE(`condition`, '=true', ''))) / LENGTH('=true')
+    WHEN 0 THEN 'info'
+    WHEN 1 THEN 'warning'
+    WHEN 2 THEN 'warning'
+    ELSE 'critical'
+    END
+);
