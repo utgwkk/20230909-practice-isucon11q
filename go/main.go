c520b835ca2747ce1dbb6cd6c259e9779386e841
utgwkk <utagawakiki@gmail.com>
2023-09-09 14:40:36 +0900

    Revert "no drop"
    This reverts commit dd8a56c9a8429a9e3b73aa5bb80db799caac08b0.

--- go/main.go
+++ go/main.go
@@ -9,6 +9,7 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"net/http"
 	"os"
 	"os/exec"
@@ -1234,6 +1235,12 @@ func getTrend(c echo.Context) error {
 // ISUからのコンディションを受け取る
 func postIsuCondition(c echo.Context) error {
 	ctx := c.Request().Context()
+	// TODO: 一定割合リクエストを落としてしのぐようにしたが、本来は全量さばけるようにすべき
+	dropProbability := 0.3
+	if rand.Float64() <= dropProbability {
+		c.Logger().Warnf("drop post isu condition request")
+		return c.NoContent(http.StatusAccepted)
+	}
 
 	jiaIsuUUID := c.Param("jia_isu_uuid")
 	if jiaIsuUUID == "" {
